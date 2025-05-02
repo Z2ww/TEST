1193c127b9cabf1cbc84eec1fe658d703ceacae0
Z2ww <17816817803@163.com>
2025-05-03 01:14:21 +0800

    Document

---
+++ src/data/tag_categories.js
@@ -0,0 +1,17 @@
+const userDefinedTagCategories = {
+	"type": {
+		"description": "类型"
+	},
+	"category": {
+		"description": "论文类别"
+	},
+	"technique": {
+		"description": "技术方法"
+	},
+	"application": {
+		"description": "应用领域"
+	},
+	"evaluation": {
+		"description": "评估方法"
+	}
+}
